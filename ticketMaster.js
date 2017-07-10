ffd02e2fd95c8493ec87670c67e266db99eb3e2e
John Son <jsson77@gmail.com>
2017-07-10 16:22:50 -0700

    appends opposing logo

--- ticketMaster.js
+++ ticketMaster.js
@@ -1,25 +1,32 @@
-function ticketMaster() {
+function addOpposingTeamLogo() {
   // Define the sigil uptop in the case that jQuery doesn't yet exist at time of script firing
-  var $;
-
+  let $;
   // Quick check for the existence of jQuery
-  if ($ = window.jQuery) init(); // jshint ignore:line
-  else {
-    var script = document.createElement("script");
-    script.src = "https://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js";
+  if ($ = window.jQuery) {
+    console.log('Sanity:jQuery exists!');
+    init(); // jshint ignore:line
+  }
 
-    script.onload = script.onreadystatechange = function () {
+  else {
+    const script = document.createElement('script');
+    script.src = "https://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js";
+    script.onload = script.onreadystatechange = () => {
       $ = window.jQuery;
       init();
     };
-
     document.body.appendChild(script);
   }
 
   // code runs when jQuery exists
-  function init(){
+  function init() {
+    const opposingLogoUrl = window._storeUtils.eventJSONData.artists[1].images[1].location;
+    // const logoDiv = '.split-content__secondary event-header__photo-container';
+    const logoDiv = '.event-header__photo-container';
+    console.log('logoDiv: ', logoDiv);
+    // $('#theDiv').prepend('<img id="theImg" src="theImg.png" />')
+    $(logoDiv).append(`<img id="event-header__photoa" src="${opposingLogoUrl}" />`);
+    // $('.split-content__secondary event-header__photo-container').prepend('<img id="theImg" src="theImg.png" />')
+  }
+}
 
-    //
-  };
-
-};
+addOpposingTeamLogo();
