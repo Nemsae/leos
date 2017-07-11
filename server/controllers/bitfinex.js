0211b2dd58a847f769a6fa81fc02b5d6cf9258a6
John Son <jsson77@gmail.com>
2017-07-10 21:03:45 -0700

    clean out comments from controller

--- server/controllers/bitfinex.js
+++ server/controllers/bitfinex.js
@@ -1,9 +1,5 @@
 const axios = require('axios');
-//  Caclulating payout
-//  1. Grab user inputs
-//  2. Get current ticker for EOS
-//  3. Compute using helper functions
-//  4. response.send(profit)
+
 exports.exchangeRate = (request, response) => {
   const { symbol } = request.query;
   const tickerUrl = `https://api.bitfinex.com/v1/pubticker/${symbol}`;
@@ -19,21 +15,16 @@ exports.exchangeRate = (request, response) => {
 
 exports.compute = (request, response) => {
   const symbol = 'eoseth';  //  eosusd, eosbtc
-  //  GET: grab ticker info for the symbol
   const tickerUrl = `https://api.bitfinex.com/v1/pubticker/${symbol}`;
-  //  GET: grab symbols
   const symbolsUrl = 'https://api.bitfinex.com/v1/symbols';
-  //  1. Grab user inputs from request
   const { myEther, projectedEth, actualEth } = request.body;
-  console.log('projectedEth: ', projectedEth);
-  console.log('actualEth: ', actualEth);
+
   let targetBenchmark = 0;
   let ethPayout = 'Not defined.';
   let eosPayout = 'Not defined.';
   let projectedEosPayout = 0;
   let projectedEthPayout = 0;
 
-  //  2. use api to grab current price of EOS
   axios.get(tickerUrl)
     .then(res => +res.data.last_price)
     .then((currExchangeRate) => {
