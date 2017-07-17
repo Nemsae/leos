b377233e80fd6df80007b3b63d93b508e66e6579
John Son <jsson77@gmail.com>
2017-07-17 15:17:58 -0700

    use paper material component for Payout page

--- client/src/components/Payout/Payout.js
+++ client/src/components/Payout/Payout.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Paper from 'material-ui/Paper';
+import RaisedButton from 'material-ui/RaisedButton';
 
 import './styles.css';
 
-export default class Home extends React.Component {
+export default class Payout extends React.Component {
   constructor () {
     super();
   }
 
   render() {
     return(
-      <div className='payout-container'>
+      <Paper className='payout-container'>
         <h1>Payout</h1>
+        <RaisedButton label='Default' />
         <Link className='button' to='/help'>
           Will Go to Records of Payout
         </Link>
-      </div>
+      </Paper>
     )
   }
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,23 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ThemeProvider from 'react-toolbox/lib/ThemeProvider';
-// import { BrowserRouter } from 'react-router-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import ThemeProvider from 'react-toolbox/lib/ThemeProvider';  //  TAKEOUT
+import injectTapEventPlugin from 'react-tap-event-plugin';
 
-import theme from './assets/react-toolbox/theme.css';
+import theme from './assets/react-toolbox/theme.css';  //  TAKEOUT
 import registerServiceWorker from './registerServiceWorker';
 import App from './components/App';
 
 import './index.css';
 
-// ReactDOM.render(
-//   <App />,
-//   document.getElementById('root') // eslint-disable-line no-undef
-// );
-// registerServiceWorker();
 ReactDOM.render(
-  <ThemeProvider theme={theme}>
-    {/* <BrowserRouter> */}
-      <App />
-    {/* </BrowserRouter> */}
-  </ThemeProvider>, document.getElementById('root'));
+  <MuiThemeProvider>
+    <App />
+  </MuiThemeProvider>, document.getElementById('root'), // eslint-disable-line no-undef
+);
+
 registerServiceWorker();
+injectTapEventPlugin();
