6ff05489975ce9c5ab5144e914e9308a6095bc00
John Son <jsson77@gmail.com>
2017-07-17 15:29:35 -0700

    setup frame of trades page

--- client/src/components/Payout/Payout.js
+++ client/src/components/Payout/Payout.js
@@ -5,20 +5,31 @@ import RaisedButton from 'material-ui/RaisedButton';
 
 import './styles.css';
 
-export default class Payout extends React.Component {
+export default class Trades extends React.Component {
   constructor () {
     super();
   }
 
   render() {
-    return(
-      <Paper className='payout-container'>
+    return (
+      <Paper className='trades-container'>
         <h1>Payout</h1>
         <RaisedButton label='Default' />
         <Link className='button' to='/help'>
           Will Go to Records of Payout
         </Link>
+        <div className='computation-container'>
+          <Paper className='benchmark-container'>
+            <h1>Benchmark</h1>
+          </Paper>
+          <Paper className='payout-container'>
+            <h1>Payout</h1>
+          </Paper>
+          <Paper className='currency-container'>
+            <h1>Current Rate</h1>
+          </Paper>
+        </div>
       </Paper>
-    )
+    );
   }
 }
