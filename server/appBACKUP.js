922cbeffa38aa8fc1197eac123d9faa3a6bb628a
John Son <jsson77@gmail.com>
2017-07-22 23:05:44 -0700

    index.ejs implement

--- server/appBACKUP.js
+++ server/appBACKUP.js
@@ -19,26 +19,6 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('../client/public'));
 }
 
-/*  EJS TEMPLATE  *//*  EJS TEMPLATE  *//*  EJS TEMPLATE  */
-app.set('view engine', 'ejs');
-
-// use res.render to load up an ejs view file
-
-//  index page
-app.get('/', (req, res) => {
-  // res.render will look in 'views' folder for the view
-  // so the full path would look like '/views/pages/index'
-  res.render('pages/index');
-});
-
-//  about page
-app.get('/about', (req, res) => {
-  res.render('pages/about');
-});
-
-/*  EJS TEMPLATE  *//*  EJS TEMPLATE  *//*  EJS TEMPLATE  */
-
-
 app.set('port', PORT);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
