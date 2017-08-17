eda88d6a8184743bedb00814d4ae52efc8ab39c2
John Son <jsson77@gmail.com>
2017-08-16 22:50:47 -0700

    change test for payout container

--- client/src/containers/Payout/Payout.spec.js
+++ client/src/containers/Payout/Payout.spec.js
@@ -8,14 +8,31 @@ import Payout from './Payout';
 import store from '../../configureStore'
 
 describe('Payout', () => {
-
-  it('should render something', () => {
+  it('should render with initial state symbol of EOSETH', () => {
     const renderer = TestUtils.createRenderer();
     renderer.render(<Payout store={store} />);  //  shallow rendering
-    const actual = renderer.getRenderOutput();
+    const actual = renderer.getRenderOutput().props.currency.symbol;
+    const expected = 'EOSETH'
     console.log('actual: ', actual);
-    // console.log('renderer.getRenderOutput(): ', renderer.getRenderOutput());
-    const expected = 'Current Rate'
+    console.log('expected: ', expected);
+    expect(actual).toEqual(expected);
+  });
+  it('should render title', () => {
+    const renderer = TestUtils.createRenderer();
+    renderer.render(<Payout store={store} />);
+    const actual = renderer.getRenderOutput();
+    const expected = <h1>Trades</h1>;
+    console.log('actual: ', actual);
+    console.log('expected: ', expected);
     expect(actual).toIncludeJSX(expected);
   });
+  // it('should render something', () => {
+  //   const renderer = TestUtils.createRenderer();
+  //   renderer.render(<Payout store={store} />);  //  shallow rendering
+  //   const actual = renderer.getRenderOutput();
+  //   console.log('actual: ', actual);
+  //   // console.log('renderer.getRenderOutput(): ', renderer.getRenderOutput());
+  //   const expected = 'Current Rate'
+  //   expect(actual).toIncludeJSX(expected);
+  // });
 });
