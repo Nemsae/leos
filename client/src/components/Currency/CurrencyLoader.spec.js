66ac123f981d0384e2da7712df64f390f06ae679
John Son <jsson77@gmail.com>
2017-08-22 22:03:16 -0700

    integrate tests for <CurrencyLoader /> for both cases where isFetching is false/true

--- client/src/components/Currency/CurrencyLoader.spec.js
+++ client/src/components/Currency/CurrencyLoader.spec.js
@@ -1,15 +1,24 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import { mount, shallow } from 'enzyme';
 import expect from 'expect';
+
 import CurrencyLoader from './CurrencyLoader';
 
-function setup() {
+function setup(bool) {
   const props = {
     rate: 0.0045,
-    isFetching: false,
+    isFetching: bool,
   };
 
-  const enzymeWrapper = mount(<CurrencyLoader {...props} />);
+  const enzymeWrapper = mount(<CurrencyLoader {...props} />, {
+    context: {
+      muiTheme: getMuiTheme(),
+    },
+    childContextTypes: {
+      muiTheme: React.PropTypes.object.isRequired,
+    },
+  });
 
   return {
     props,
@@ -17,10 +26,22 @@ function setup() {
   };
 }
 
-describe('CurrencyLoader component', () => {
-  it('should render self and subcomponents', () => {
-    const { enzymeWrapper } = setup();
+describe('<CurrencyLoader />', () => {
+  it('should render <p> with rate when isFetching is false', () => {
+    const { enzymeWrapper } = setup(false);
     expect(enzymeWrapper.find('div').hasClass('currency-rate')).toBe(true);
     expect(enzymeWrapper.find('p').hasClass('currency-rate__text')).toBe(true);
+  });
+
+  it('should render the spinner when isFetching is true', () => {
+    const { enzymeWrapper } = setup(true);
+    const html = enzymeWrapper.html();
+    expect(enzymeWrapper.find('div').first().hasClass('currency-rate')).toBe(true);
+
+    //  Props check
+    const CircularProgressProps = enzymeWrapper.find('CircularProgress').props();
+    expect(CircularProgressProps.color).toBe('white');
+    expect(CircularProgressProps.size).toBe(30);
+    // expect(enzymeWrapper.find('CircularProgress').hasClass('currency-rate__text')).toBe(true);
   });
 });
