c5e9f326c500664951100b6bb9df302642f69794
John Son <jsson77@gmail.com>
2017-08-24 21:19:05 -0700

    refactor and updated <FilterLink /> g

--- client/src/components/FilterLink/FilterLink.js
+++ client/src/components/FilterLink/FilterLink.js
@@ -2,39 +2,33 @@ import React from 'react';
 
 import './styles.css';
 
-class FilterLink extends React.Component {
-  shouldComponentUpdate(nextProps) {
-    // console.log('nextProps.filter: ', nextProps.filter);
-    if (nextProps.filter !== this.props.filter) {
-      // console.log('this.props.filter: ', this.props.filter);
-      return true;
-    }
-  }
+/* props.filter DOES CHANGE HERE */
+// class FilterLink extends React.Component {
+//   render() {
+//     console.log('this.props.filter: ', this.props.filter);
+//     console.log('this.props.filterType: ', this.props.filterType);
+//     console.log(this.props.filter === this.props.filterType);
+//     return (
+//       this.props.filter === this.props.filterType ?
+//         <span className='active-link'>{this.props.children}</span>
+//         :
+//         <a id={this.props.filterType} className='link' href='' onClick={this.props.setFilter}>
+//           {this.props.children}
+//         </a>
+//     );
+//   }
+// }
 
-  render() {
-    console.log('props.filter: ', this.props.filter);
-    console.log('this.props.filterType: ', this.props.filterType);
-    console.log(this.props.filter === this.props.filterType);
-    return (
-      this.props.filter === this.props.filterType ?
-        <span className='active-link'>{this.props.children}</span>
-        :
-        <a id={this.props.filterType} className='link' href='' onClick={this.props.setFilter}>
-          {this.props.children}
-        </a>
-    );
-    // if (props.filter === props.filterType) {
-    //   console.log('props.filter: ', props.filter);
-    //   console.log('props.filterType: ', props.filterType);
-    //   return <span className='active-link'>{props.children}</span>;
-    // }
-    // return (
-    //   <a id={props.filterType} className='link' href='' onClick={props.setFilter}>
-    //     {props.children}
-    //   </a>
-    // );
-  }
-};
+const FilterLink = props => (
+  props.filter === props.filterType ?
+    <span className='active-link'>{props.children}</span>
+    :
+    <a id={props.filterType} className='link' href='' onClick={props.setFilter}>
+      {props.children}
+    </a>
+);
+
+// /* props.filter DOESN'T CHANGE HERE */
 // const FilterLink = props => ({
 //   render() {
 //     console.log('props.filter: ', props.filter);
@@ -48,16 +42,6 @@ class FilterLink extends React.Component {
 //           {props.children}
 //         </a>
 //     );
-//     // if (props.filter === props.filterType) {
-//     //   console.log('props.filter: ', props.filter);
-//     //   console.log('props.filterType: ', props.filterType);
-//     //   return <span className='active-link'>{props.children}</span>;
-//     // }
-//     // return (
-//     //   <a id={props.filterType} className='link' href='' onClick={props.setFilter}>
-//     //     {props.children}
-//     //   </a>
-//     // );
 //   },
 // });
 
