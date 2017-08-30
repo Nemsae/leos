1db3f1e50f4d48e4330a1f82d62a391f65e3c06d
John Son <jsson77@gmail.com>
2017-08-29 22:14:58 -0700

    abstract out <Returns /> from <Payout />

--- client/src/containers/Payout/Payout.js
+++ client/src/containers/Payout/Payout.js
@@ -13,6 +13,7 @@ import './styles.css';
 /* Components */
 // import Icon from '../../components/Icon';
 import Benchmark from '../../components/Benchmark';
+import Returns from '../../components/Returns';
 import Currency from '../../components/Currency';
 // import { CurrencyLoader } from '../../components/CurrencyLoader';
 // import { CurrencyFooter } from '../../components/CurrencyFooter';
@@ -62,9 +63,7 @@ export class Payout extends React.Component {
         </Link>
         <div className='computation-container'>
           <Benchmark />
-          <Paper className='payout-container'>
-            <h1>Payout</h1>
-          </Paper>
+          <Returns />
           <Currency
             symbol={this.state.symbol}
             handleCurrencyChange={this.handleCurrencyChange}
