02f399b4451119c5c03bbdeccbd3ebb145ee5b58
John Son <jsson77@gmail.com>
2017-08-30 23:59:47 -0700

    add test

--- client/src/components/Returns/Returns.spec.js
+++ client/src/components/Returns/Returns.spec.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import expect from 'expect';
 
+import Returns from './Returns';
+
 describe('<Returns/>', () => {
   it('should render self and subcomponents', () => {
-
+    
   });
 });
